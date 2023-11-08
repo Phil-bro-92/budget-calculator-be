822489628484f2eb0813bb1a9ff04085613ff228
Phil Broadley <philbrocoding@hotmail.com>
2023-11-08 21:24:10 +0000

    login

--- app.js
+++ app.js
@@ -6,20 +6,47 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+// DB connection
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect("mongodb://127.0.0.1:27017", { useUnifiedTopology: true })
+    .then(client => {
+        const db = client.db("budget-calculator");
+        const customersCollection = db.collection("customers");
+
+        app.use("/customers/login", async (req, res) => {
+            const customers = await customersCollection.find({}).toArray();
+            const data = req.body;
+            for (let i = 0; i < customers.length; i++) {
+                if (
+                    data.email === customers[i].email &&
+                    data.password === customers[i].password
+                ) {
+                    res.json({ message: "User exists" });
+                } else {
+                    res.json({ message: "User does not exist" });
+                }
+            }
+        });
+    })
+    .catch(error => {
+        console.error("Error connecting to MongoDB:", error);
+    });
+
 app.get("/", async (req, res) => {
-  console.log("HELLO WORLD");
-  res.status(200);
-  res.send({ message: "Hello World" });
+    console.log("HELLO WORLD");
+    res.status(200);
+    res.send({ message: "Hello World" });
 });
 
 app.post("/submit", async (req, res) => {
-  res.status(200);
-  res.send("Success");
-  console.log(req.body);
+    res.status(200);
+    res.send("Success");
+    console.log(req.body);
 });
 
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}.`);
+    console.log(`App is listening on port ${PORT}.`);
 });
