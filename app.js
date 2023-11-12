3c5ceb7f91e15f1d3164bc1245bccd2a84dd989a
Phil Broadley <philbrocoding@hotmail.com>
2023-11-12 21:26:25 +0000

    various

--- app.js
+++ app.js
@@ -2,47 +2,131 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ObjectId } = require("mongodb");
+const bcrypt = require("bcrypt");
 
+//Middleware
 app.use(cors());
 app.use(express.json());
 
-// DB connection
-const MongoClient = require("mongodb").MongoClient;
-
-MongoClient.connect("mongodb://127.0.0.1:27017", { useUnifiedTopology: true })
-    .then(client => {
-        const db = client.db("budget-calculator");
-        const customersCollection = db.collection("customers");
-
-        app.use("/customers/login", async (req, res) => {
-            const customers = await customersCollection.find({}).toArray();
-            const data = req.body;
-            for (let i = 0; i < customers.length; i++) {
-                if (
-                    data.email === customers[i].email &&
-                    data.password === customers[i].password
-                ) {
-                    res.json({ message: "User exists" });
-                } else {
-                    res.json({ message: "User does not exist" });
-                }
-            }
-        });
-    })
-    .catch(error => {
-        console.error("Error connecting to MongoDB:", error);
+//Connect to DB
+async function connect() {
+    const client = new MongoClient("mongodb://127.0.0.1:27017", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
 
-app.get("/", async (req, res) => {
-    console.log("HELLO WORLD");
-    res.status(200);
-    res.send({ message: "Hello World" });
+    try {
+        await client.connect();
+        console.log("Connected to the database");
+        return client.db("budget-calculator");
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        throw error;
+    }
+}
+
+//ROUTES
+
+//Get all customers
+app.get("/customers", async (req, res) => {
+    try {
+        const db = await connect();
+        const customers = await db.collection("customers").find().toArray();
+        res.json(customers);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
-app.post("/submit", async (req, res) => {
-    res.status(200);
-    res.send("Success");
-    console.log(req.body);
+//Get customer by ID
+app.get("/customers/:id", async (req, res) => {
+    try {
+        //Connect to DB
+        const db = await connect();
+        // Extract the customer ID from the request parameters
+        const customerId = req.params.id;
+        console.log(customerId);
+        // Validate the ID format
+        if (!ObjectId.isValid(customerId)) {
+            return res
+                .status(400)
+                .json({ error: "Invalid customer ID format" });
+        }
+
+        // Find the customer by ID
+        const customer = await db
+            .collection("customers")
+            .findOne({ _id: new ObjectId(customerId) });
+
+        // Check if the customer exists
+        if (!customer) {
+            return res.status(404).json({ error: "Customer not found" });
+        }
+
+        // Send the customer data in the response
+        res.json(customer);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+//Register new customer
+app.post("/register", async (req, res) => {
+    try {
+        const customer = req.body;
+        //Connect to DB
+        const db = await connect();
+        //Get current list of customers
+        const currentCustomers = await db
+            .collection("customers")
+            .find()
+            .toArray();
+        //Check new customer is not already registered
+        const isEmailRegistered = currentCustomers.some(
+            customer => customer.email === req.body.email
+        );
+
+        if (isEmailRegistered) {
+            // Don't add if email is already registered
+            console.log("Email is already registered.");
+            res.status(400).json({ message: "Email is already registered" });
+        } else {
+            // Add the new customer
+            await db.collection("customers").insertOne(customer);
+            console.log("New customer added.");
+            res.status(201).json({ message: "Customer added successfully" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+//Login customer
+app.post("/login", async (req, res) => {
+    try {
+        //Connect to DB
+        const db = await connect();
+        //Extract body
+        const user = req.body;
+        console.log(user);
+        // Find the customer by email
+        const customer = await db.collection("customers").findOne(user);
+        console.log(customer);
+        // Check if the customer exists
+        if (!customer) {
+            return res.status(404).json({ error: "Customer not found" });
+        }
+
+        // If everything is okay, you can return the customer data or a token for authentication
+        res.json(customer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 const PORT = process.env.PORT || 9000;
